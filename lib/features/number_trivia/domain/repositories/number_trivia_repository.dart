d9c8c2464765dfe22fb36bb88407329e632e351a
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-01-10 14:59:57 +0200

    added NumberTriviaRepository abstract class

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:trivia_app/core/failure.dart';
+import 'package:trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
